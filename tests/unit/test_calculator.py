932523b0cdd0e5d0e82465948da772f91ba68cfa
Karthik Krapa <karthik.krapa18@Karthiks-MacBook-Air.local>
2025-02-10 00:03:16 -0500

    Added Tests for Calculator

---
+++ tests/unit/test_calculator.py
@@ -0,0 +1,13 @@
+from src.components.calculator import Calculator
+
+def test_add():
+    calc = Calculator()
+    assert calc.add(2, 3) == 5
+
+def test_subtract():
+    calc = Calculator()
+    assert calc.subtract(5, 3) == 2
+
+def test_multiply():
+    calc = Calculator()
+    assert calc.multiply(4, 2) == 8
